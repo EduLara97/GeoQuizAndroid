25786a219f7f3b594d0d10bd1bbb1905d0e6f335
Edwin Lara <edu.lara.lev@gmail.com>
2017-08-17 23:49:54 -0500

    primer commit

---
+++ app/src/main/java/com/bignerdranch/android/geoquiz/Question.java
@@ -0,0 +1,32 @@
+package com.bignerdranch.android.geoquiz;
+
+/**
+ * Created by Edwin on 4/08/2017.
+ */
+
+public class Question {
+
+    private int mTextResID;
+    private boolean mAnswerTrue;
+
+    public Question(int mTextResID, boolean mAnswerTrue) {
+        this.mTextResID = mTextResID;
+        this.mAnswerTrue = mAnswerTrue;
+    }
+
+    public int getTextResID() {
+        return mTextResID;
+    }
+
+    public void setTextResID(int textResID) {
+        mTextResID = textResID;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+}
